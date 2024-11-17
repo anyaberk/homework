eebcfab88bd4a6f4f8fc790f5e49fc17fab2a001
anyaberk <anyaber.0@gmail.com>
2024-11-17 15:39:15 +0300

    task

--- homework3/task14.py
+++ homework3/task14.py
@@ -4,6 +4,28 @@
 # Пример:
 mass = [1,2,17,54,30,89,2,1,6,2]
 
+from random import randint as ri
+n=int(input('Введите размер массива: '))
+mass=[None]*n
+for i in range(n):
+    mass[i]=ri(0,10)
+print(mass)
+
+min_distance = n
+min_indices = [None]*2
+
+for i in range(len(mass)):
+    for j in range(i + 1, len(mass)):
+        if mass[i] == mass[j]:
+            distance = j - i
+            if distance < min_distance:
+                min_distance = distance
+                min_indices = [i, j]
+
+if min_indices[0] != None:
+    print(f"Минимальное расстояние: {min_distance}, Индексы: {min_indices}")
+else:
+    print('Одинаковых чисел в массиве нет')
 
 # Для числа 1 минимальное растояние в массиве по индексам: 0 и 7
 # Для числа 2 минимальное растояние в массиве по индексам: 6 и 9
